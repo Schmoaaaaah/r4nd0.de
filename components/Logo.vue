eedc893d7ad38750e80810946901db7e94779abd
Noah <nkempers@outlook.de>
2021-01-02 17:42:29 +0100

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <span id="anim"></span>
+    <div id="logo">
+      <div class="text">
+        <p>Nothing to see here!</p>
+      </div>
+      <img src="logo.png" class="transformed" />
+      <img src="logo.png" />
+    </div>
+  </div>
+</template>
+
+<style>
+img {
+  width: 100%;
+  height: auto;
+  margin-left: auto;
+  box-shadow: 0 0 5rem gray;
+}
+#anim {
+  display: none;
+  position: absolute;
+  animation: anim 20s infinite ease-in-out;
+  background-color: black;
+}
+
+#logo {
+  position: relative;
+  perspective: 1000px;
+  cursor: pointer;
+}
+
+#logo > .text {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: calc(100% - 2rem);
+  height: calc(50% - 2rem);
+  padding: 1rem;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+#logo > .transformed {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  clip-path: inset(0 0 50% 0);
+  transition: 1s;
+}
+
+#logo::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 50%;
+  width: 100%;
+  height: 0;
+  background-color: brown;
+  transition: 1s;
+  opacity: 0.1;
+}
+
+#logo:hover::after {
+  height: 0.1rem;
+}
+
+#logo:hover > .transformed {
+  transform: rotateX(-70deg);
+  transform-origin: 50% 50%;
+  border-bottom: 5px solid black;
+}
+
+@keyframes anim {
+  0% {
+    left: 0;
+    top: 0;
+    width: 0.5rem;
+    height: 0.5rem;
+  }
+
+  15% {
+    width: 5rem;
+  }
+  25% {
+    left: calc(100% - 0.5rem);
+    top: 0;
+    width: 0.5rem;
+    height: 0.5rem;
+  }
+
+  40% {
+    height: 5rem;
+  }
+  50% {
+    left: calc(100% - 0.5rem);
+    top: calc(100% - 0.5rem);
+    height: 0.5rem;
+    width: 0.5rem;
+  }
+
+  65% {
+    width: 5rem;
+  }
+  75% {
+    left: 0%;
+    top: calc(100% - 0.5rem);
+    height: 0.5rem;
+    width: 0.5rem;
+  }
+
+  90% {
+    height: 5rem;
+  }
+  100% {
+    left: 0%;
+    top: 0%;
+    height: 0.5rem;
+    width: 0.5rem;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <div class="text-center my-12">
+          <logo />
+        </div>
+        <v-card class="my-12">
+          <v-card-title class="headline"> We’ll be back soon! </v-card-title>
+          <v-card-text>
+            <p>
+              Sorry for the inconvenience but we’re performing some maintenance
+              at the moment. If you need to you can always contact us, otherwise
+              we’ll be back online shortly!
+            </p>
+            <p>— The Team</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="primary" nuxt to="/about"> Continue </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title> In the mean time... </v-card-title>
+          <v-card-text>
+            <p>You may want to join us on Discord?</p>
+            <iframe
+              src="https://ptb.discord.com/widget?id=435056107371888640&theme=dark"
+              width="350"
+              height="500"
+              allowtransparency="true"
+              frameborder="0"
+              sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+            ></iframe>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo,
+  },
+}
+</script>
