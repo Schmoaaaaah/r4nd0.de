6fa643dbcc70a2045a8810733ae4520d48c86695
Noah Kempers <nkempers@r4nd0.de>
2022-06-17 15:58:45 +0200

    migrate Nuxt 3

---
+++ src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/projects",
+    name: "projects",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/ProjectsView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/logo.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;First, get some Content. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Me two Hours in two this site</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
